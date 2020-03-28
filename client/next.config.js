f1369e2c5d05ef0f2e20055f93e7e9ef740c791a
jahnvi <jbdalwadi01@gmail.com>
2020-03-28 13:37:46 +0530

    Update next.config.js

--- client/next.config.js
+++ client/next.config.js
@@ -6,7 +6,7 @@ module.exports=withCSS({
         PRODUCTION:false,
         DOMAIN_DEVELOPMENT:'http://localhost:3000',
         DOMAIN_PRODUCTION:'https://seoblog.com',
-        FB_APP_ID:'3074888912563765',
+        
         
     }
 })
